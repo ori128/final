1aa32da63d360a53085618fa887548efff49f229
Student-2024 <student-2024@ashalim.org>
2025-11-27 09:00:59 +0200

    init

--- app/src/main/java/com/ori/afinal/MainActivity.java
+++ app/src/main/java/com/ori/afinal/MainActivity.java
@@ -1,37 +1,38 @@
 package com.ori.afinal;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.play.core.integrity.IntegrityTokenRequest;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private Button btnRegister, btnLogin;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
+        btnRegister = findViewById(R.id.btnRegister);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+        btnRegister.setOnClickListener(this);
     }
 
-    public void GO(View view) {
-        Intent intent = new Intent(MainActivity.this, RegisterUser.class );
-        startActivity(intent);
-        finish();
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==btnLogin.getId())
+        {
+            Intent intent = new Intent(MainActivity.this,Login.class);
+            startActivity(intent);
+        }
+        else if (view==btnRegister)
+        {
+            Intent intent = new Intent(MainActivity.this, RegisterUser.class);
+            startActivity(intent);
+        }
     }
 }
